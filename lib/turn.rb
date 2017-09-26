d28ab7dcc8846904b7cde40fcd9588e86e2e5ac2
Jonathan Rosado <jon.rosado42@gmail.com>
2017-09-26 23:09:00 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,7 +5,7 @@ def turn(board)
 
   index = input_to_index(user_input)
 
-  if valid_move?(board, index) == true && position_taken?(board, index) == true
+  if valid_move?(board, index) == true
 
       move(board, index)
 
